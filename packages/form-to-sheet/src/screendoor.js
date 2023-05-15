0c47414c2e2d2ee9af9f6fbf40a0d6355521e53f
John Dunning <fw@johndunning.com>
2023-05-14 19:22:35 -0700

    Add CLI args to form-to-sheet
    Add glob and minimist to manage args.

--- packages/form-to-sheet/src/screendoor.js
+++ packages/form-to-sheet/src/screendoor.js
@@ -1,9 +1,21 @@
+import { join, basename, dirname } from "node:path";
 import fs from "fs-extra";
+import parseArgs from "minimist";
+import { globSync } from "glob";
 import jmespath from "jmespath";
 import { writeToPath } from "fast-csv";
 import * as XLSX from "xlsx";
 
 XLSX.set_fs(fs);
+
+function getArgs(
+	argString = process.argv.slice(2))
+{
+	const { _: [command], ...flags } = parseArgs(argString);
+	const { input, output = "Screendoor.xlsx" } = flags;
+
+	return { command, flags: { input, output } };
+}
 
 const Headers = ["ID", "Type", "Label", "Required", "Options"];
 
@@ -16,11 +28,14 @@ const keys = (keyString) => `.{${
 }}`;
 const { search } = jmespath;
 
-const allForms = fs.readJsonSync("screendoor.json");
+const { flags: { input, output } } = getArgs();
+	// since join will use \ in the path, we have to tell glob to ignore \ for escaping
+const formPaths = globSync(join(input, "**/forms.json"), { windowsPathsNoEscape: true });
 const wb = { SheetNames: [], Sheets: {} };
-const name = "equity-incubator";
 
-allForms.forEach((form) => {
+formPaths.forEach((formPath) => {
+	const [form] = fs.readJsonSync(formPath);
+	const name = basename(dirname(formPath)).slice(0, 31);
 	const { field_data } = form;
 	const rows = [Headers];
 
@@ -29,7 +44,9 @@ allForms.forEach((form) => {
 		let optionsText = "";
 
 		if (options && options.length) {
-			optionsText = search(field, "options[*].label").join("\n");
+			optionsText = search(field, "options[].label")
+				.map((label) => `'${label}'`)
+				.join("\n");
 		}
 
 		rows.push([id, field_type, label, required, optionsText]);
@@ -41,9 +58,6 @@ allForms.forEach((form) => {
 
 	wb.SheetNames.push(name);
 	wb.Sheets[name] = ws;
-
-//	fs.writeJsonSync(`${name}.json`, rows);
-//	writeToPath(`${name}.csv`, rows);
 });
 
-XLSX.writeFile(wb, "Screendoor.xlsx");
+XLSX.writeFile(wb, output);
